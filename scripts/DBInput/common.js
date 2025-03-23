edf6f19c630555cb068c6eb1cfe7614ba9ee8d25
NicholasZanetiAulivier <nicholas.535240054@stu.untar.id>
2025-03-23 12:13:53 +0700

    Added encounter-method.js

--- scripts/DBInput/common.js
+++ scripts/DBInput/common.js
@@ -9,19 +9,30 @@ function mongooseInit(){
     );
 }
 
+let commonModels = {};
+
+commonModels.language = {
+    name : String,
+    id : Number
+}
+
+commonModels.names = [{
+    name : String,
+    language : commonModels.language
+}]
+
+commonModels.NamedAPIResource = {
+    name : String,
+    id : Number
+}
+
 let schemas = {}
 
 schemas.encounter_method_schema = {
     id : Number,
     name : String,
     order : Number,
-    names : [{
-        name : String,
-        language : {
-            name : String,
-            id : Number
-        }
-    }]
+    names : commonModels.names
 } ;
 
 schemas.language_schema = {
@@ -30,14 +41,23 @@ schemas.language_schema = {
     official : Boolean,
     iso639 : String,
     iso3166 : String,
-    names : [{
-        name : String,
-        language : {
-            name : String,
-            id : Number
-        }
-    }]
+    names : commonModels.names
 } 
+
+schemas.contest_effect_schema = {
+    id : Number,
+    appeal : Number,
+    jam : Number,
+    effect_entries : [{
+        effect : String,
+        language : commonModels.language
+    }],
+    flavor_text_entries : [{
+        flavor_text : String,
+        language : commonModels.language
+    }]
+}
 
 exports.mongooseInit = mongooseInit;
 exports.schemas = schemas;
+exports.commonModels = commonModels;
---
+++ scripts/DBInput/contest-effect.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+let common = require('./common');
+
+common.mongooseInit();
+
+//Real code starts here
+//Change schema,model, url, and processData()
+
+let mainURL = "https://pokeapi.co/api/v2/contest-effect";
+let ULen = mainURL.length+1;
+
+const contestEffectSchema = mongoose.Schema(common.schemas.contest_effect_schema , {collection : "contesteffect"});
+
+const ContestEffect = mongoose.model("ContestEffect", contestEffectSchema);
+
+async function processData(data) {
+    for (let i in data.effect_entries){
+        data.effect_entries[i].language.id = data.effect_entries[i].language.url.slice(ULen,-1);
+        delete data.effect_entries[i].language.url;
+    }
+    for (let i in data.flavor_text_entries){
+        data.flavor_text_entries[i].language.id = data.flavor_text_entries[i].language.url.slice(ULen,-1);
+        delete data.flavor_text_entries[i].language.url;
+        if (data.flavor_text_entries[i].version) {delete data.flavor_text_entries[i].version;}
+    }
+    ContestEffect.create(data).then(u => console.log(u));
+}
+
+//main code
+async function startInput(){
+    let index = `${mainURL}?limit=100000`;
+    let listOfItems = await fetch(index).then((res)=> res.json());
+    for (let i of listOfItems.results){
+        let tempURL = `${mainURL}/${i.url.slice(ULen,-1)}/`;
+        let data = await fetch(tempURL).then(res => res.json());
+        processData(data);
+    }
+}
+
+startInput().then(()=>setTimeout(()=>{mongoose.connection.close();console.log("Done");},1000));
