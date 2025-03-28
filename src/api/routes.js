a3b481357c63f5ada88f97fbf76538411e2c3d71
NicholasZanetiAulivier <nicholas.535240054@stu.untar.id>
2025-03-28 13:03:12 +0700

    Fixed non-existent books model

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
-const books = require('./components/books/books-route');
+// const books = require('./components/books/books-route');
 const template = require('./components/template/route');
 
 module.exports = () => {
     const app = express.Router();
 
-    books(app);
+    // books(app);
     template(app);
 
     return app;
