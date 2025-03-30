fe0093660c4e4bd6a439405cddf6591b1876252c
NicholasZanetiAulivier <nicholas.535240054@stu.untar.id>
2025-03-30 15:53:01 +0700

    Fixed memory leak in DBPopulate.js

--- src/scripts/oneInput.js
+++ src/scripts/oneInput.js
@@ -3,7 +3,7 @@ const wrapper = require('../utils/wrapper');
 
 const mainURL = 'https://pokeapi.co/api/v2/';
 
-const testModel = 'pokemon-form';
+const testModel = 'move';
 
 async function processData(data, model) {
     wrapper(data);
