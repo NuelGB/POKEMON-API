1c4739ccae986f8e3216a570d1b4cd28639ea913
NicholasZanetiAulivier <nicholas.535240054@stu.untar.id>
2025-04-02 14:47:35 +0700

    evochain repo fix 2

--- src/api/components/evolution-chain/evolution-chain-repository.js
+++ src/api/components/evolution-chain/evolution-chain-repository.js
@@ -1,17 +1,20 @@
 const evoChain = require('../../../models')['evolution-chain'];
 
 async function getByID(id) {
-    //Filter all 3 layers of chainlink to rid of _id
-    return evoChain.find({id} , {
-        _id :0,
-        __v:0,
-        "chain.evolves_to.evolution_details._id":0,
-        "chain.evolves_to._id" : 0,
-        "chain.evolves_to.evolves_to._id" : 0,
-        "chain.evolves_to.evolves_to.evolution_details._id" : 0,
-        "chain.evolves_to.evolves_to.evolves_to._id" : 0,
-        "chain.evolves_to.evolves_to.evolves_to.evolution_details._id" : 0,
-    });
+    // Filter all 3 layers of chainlink to rid of _id
+    return evoChain.find(
+        { id },
+        {
+            '_id': 0,
+            '__v': 0,
+            'chain.evolves_to.evolution_details._id': 0,
+            'chain.evolves_to._id': 0,
+            'chain.evolves_to.evolves_to._id': 0,
+            'chain.evolves_to.evolves_to.evolution_details._id': 0,
+            'chain.evolves_to.evolves_to.evolves_to._id': 0,
+            'chain.evolves_to.evolves_to.evolves_to.evolution_details._id': 0,
+        }
+    );
 }
 
 module.exports = {
