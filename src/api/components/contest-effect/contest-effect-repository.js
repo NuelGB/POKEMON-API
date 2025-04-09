ab6bfeafb2a76216a6169ff4a4e23d03b7fef63a
NicholasZanetiAulivier <nicholas.535240054@stu.untar.id>
2025-04-09 12:57:16 +0700

    Added /contest-type/ endpoint

--- src/api/components/contest-effect/contest-effect-repository.js
+++ src/api/components/contest-effect/contest-effect-repository.js
@@ -1,12 +1,15 @@
 const contestEffect = require('../../../models')['contest-effect'];
 
 async function getByID(id) {
-    return contestEffect.find({ id },{
-        '_id' : 0,
-        '__v' : 0,
-        'effect_entries._id' : 0,
-        'flavor_text_entries._id' : 0,
-    });
+    return contestEffect.find(
+        { id },
+        {
+            '_id': 0,
+            '__v': 0,
+            'effect_entries._id': 0,
+            'flavor_text_entries._id': 0,
+        }
+    );
 }
 
 module.exports = {
--- src/api/routes.js
+++ src/api/routes.js
@@ -5,6 +5,7 @@ const pagination = require('./components/pagination/pagination-route');
 const evoChain = require('./components/evolution-chain/evolution-chain-route');
 const berry = require('./components/berry/berry-route');
 const contestEffect = require('./components/contest-effect/contest-effect-route');
+const contestType = require('./components/contest-type/contest-type-route');
 
 module.exports = () => {
     const app = express.Router();
@@ -14,6 +15,7 @@ module.exports = () => {
     evoChain(app);
     berry(app);
     contestEffect(app);
+    contestType(app);
 
     return app;
 };
