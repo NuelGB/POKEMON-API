be0cc2ec46a7d52ba1473cc3ae35811365ce2d00
NicholasZanetiAulivier <nicholas.535240054@stu.untar.id>
2025-04-13 20:07:10 +0700

    Added /language/languages endpoint

--- src/api/components/language/language-controller.js
+++ src/api/components/language/language-controller.js
@@ -1,4 +1,5 @@
 const service = require('./language-service');
+const { errorResponder, errorTypes } = require('../../../core/errors');
 
 async function get(request, response, next) {
     try {
@@ -12,6 +13,43 @@ async function get(request, response, next) {
     }
 }
 
+async function get(request, response, next) {
+    try {
+        const { str } = request.params;
+
+        const postedDocument = await service.getItem(str);
+
+        return response.status(200).json(postedDocument);
+    } catch (error) {
+        return next(error);
+    }
+}
+
+async function getList(request, response, next) {
+    try {
+        const offset = Number(request.query.offset) || 0;
+        const limit = Number(request.query.limit) || 20;
+
+        if (offset < 0 || limit < 0) {
+            throw errorResponder(
+                errorTypes.ARGUMENT_TYPE,
+                `Query arguments cannot be negative!`
+            );
+        }
+
+        const doc = await service.getList(offset, limit);
+
+        return response.status(200).json({
+            offset,
+            limit,
+            data: doc,
+        });
+    } catch (error) {
+        return next(error);
+    }
+}
+
 module.exports = {
     get,
+    getList,
 };
