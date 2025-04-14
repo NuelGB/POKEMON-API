f1b22f919396232963ccec505472312442b7432e
chris88-88 <christhio.535240050@stu.untar.ac.id>
2025-04-14 12:30:37 +0700

    Punya Christhio

--- src/api/components/characteristic/characteristic-repository.js
+++ src/api/components/characteristic/characteristic-repository.js
@@ -1,18 +1,13 @@
-const characteristic = require('../../../models')['characteristic-chain'];
+const characteristic = require('../../../models')['characteristic'];
 
 async function getByID(id) {
     // Filter all 3 layers of chainlink to rid of _id
     return characteristic.find(
         { id },
         {
-            // '_id': 0,
-            // '__v': 0,
-            // 'descriptions._id': 0,
-            // 'descriptions.language._id': 0,
-            // 'names._id': 0,
-            // 'names.language._id': 0,
-            // 'types._id': 0,
-            // 'types.abilities._id': 0,
+            '_id': 0,
+            '__v': 0,
+            'descriptions._id': 0,
         }
     );
 }
