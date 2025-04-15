65dc22fd5f0010d8691106c06bf205c82aa59258
Jeremia <jeremiap787@gmail.com>
2025-04-15 18:03:14 +0700

    Change the use of isNaN to Number.isNaN to eliminate the eslint problem

--- src/api/components/pokemon-form/pokemon-form-service.js
+++ src/api/components/pokemon-form/pokemon-form-service.js
@@ -5,8 +5,7 @@ async function getPokemonForm(offset, limit) {
 }
 
 async function getBy(other) {
-    // eslint-disable-next-line no-restricted-globals
-    if (!isNaN(Number(other))) {
+    if (Number.isNaN(Number(other))) {
         return repository.getByID(Number(other));
     }
     return repository.getByName(other);
