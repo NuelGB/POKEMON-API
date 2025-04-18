9bf0c6a8e520a53af638cee3ec9edcc64dc716c0
NicholasZanetiAulivier <nicholas.535240054@stu.untar.id>
2025-04-18 13:39:58 +0700

    Addded /growth-rate/growth-rates endpoint

--- src/api/components/growth-rate/growth-rate-route.js
+++ src/api/components/growth-rate/growth-rate-route.js
@@ -6,5 +6,7 @@ const route = express.Router();
 
 module.exports = (app) => {
     app.use('/growth-rate', route);
+
+    route.get('/growth-rates' , controller.getList);
     route.get('/:str', controller.getBy);
 };
