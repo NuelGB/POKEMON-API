96d689b3053ee0618b2558aa666d370567c2d2ba
NicholasZanetiAulivier <nicholas.535240054@stu.untar.id>
2025-04-18 15:15:50 +0700

    Added name choice for params to ability

--- src/api/components/ability/ability-repository.js
+++ src/api/components/ability/ability-repository.js
@@ -1,9 +1,25 @@
 const { ability } = require('../../../models');
 
 async function getByID(id) {
-    // Filter all 3 layers of chainlink to rid of _id
-    return ability.find(
+    return ability.findOne(
         { id },
+        {
+            '_id': 0,
+            '__v': 0,
+            'effect_changes.effect_entries._id': 0,
+            'effect_changes._id': 0,
+            'effect_entries._id': 0,
+            'flavor_text_entries._id': 0,
+            'names._id': 0,
+            'generation._id': 0,
+            'pokemon._id': 0,
+        }
+    );
+}
+
+async function getByName(name) {
+    return ability.findOne(
+        { name },
         {
             '_id': 0,
             '__v': 0,
@@ -40,5 +56,6 @@ async function getList(offset, limit) {
 
 module.exports = {
     getByID,
+    getByName,
     getList,
 };
